1064c5d27e0a1ec5d456de6191a42323de74e572
HARSH ASHOK ATHAVALE <harshathavale190920@gmail.com>
2023-05-03 16:22:28 +0530

    Completed App

---
+++ src/components/Board.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Box from "./Box";
+import "./Board.css";
+const Board = ({ board, onClick }) => {
+  return (
+    <div className="board">
+      {board.map((value, index) => (
+        <Box value={value} onClick={() => value === null && onClick(index)} />
+      ))}
+    </div>
+  );
+};
+
+export default Board;
